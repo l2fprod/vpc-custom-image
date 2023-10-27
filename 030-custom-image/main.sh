6019c187603d75412f8f89c799c25e2e135051c2
Frederic Lavigne <frederic.lavigne@ibm.com>
2023-10-27 15:48:58 -0500

    name constraints have changed

--- 030-custom-image/main.sh
+++ 030-custom-image/main.sh
@@ -8,7 +8,7 @@ fi
 # initialize variable used by the Packer build
 export PKR_VAR_ibmcloud_api_key=$TF_VAR_ibmcloud_api_key
 export PKR_VAR_region=$TF_VAR_region
-export PKR_VAR_image_name=$(cd ../020-prepare-custom-image && terraform output -raw basename)-image-$(date +%Y-%m-%d-%H-%M-%S.%N)
+export PKR_VAR_image_name=$(cd ../020-prepare-custom-image && terraform output -raw basename)-$(date +%Y-%m-%d-%H-%M-%S-%N)
 export PKR_VAR_subnet_id=$(cd ../020-prepare-custom-image && terraform output -raw subnet_id)
 export PKR_VAR_resource_group_id=$(cd ../020-prepare-custom-image && terraform output -raw resource_group_id)
 export PKR_VAR_security_group_id=$(cd ../020-prepare-custom-image && terraform output -raw security_group_id)
